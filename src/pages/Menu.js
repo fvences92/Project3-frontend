e619f115476b6ae30e021e8a34c91ebb318e8f38
PhredE <phrede@PhredEs-MacBook-Air.local>
2022-02-01 08:27:57 -0800

    corrected Menu page

--- src/pages/Menu.js
+++ src/pages/Menu.js
@@ -1,5 +1,39 @@
-function Menu(props) {
-    return<h1>Menu Page</h1>
-}
+
+// IMPORT LINK
+import { Link } from "react-router-dom";
+
+
+const Menu = (props) => {
+    // Loaded Function
+    const loaded = () => {
+        return props.foods.map((eachFood) => (
+            <div className="home-container">
+                <div key={eachFood._recipieName} className="home-inner-container">
+                    <div className="image-link">
+                        <Link to={`/food/${eachFood._recipieName}`} className="link-element"><img src={eachFood.recipieThumb} alt={eachFood.recipieName} /></Link>
+                    </div>
+                    <Link to={`/food/${eachFood._recipieName}`} className="link-element"><div className="title">
+                        <h3>{eachFood._recipieName}</h3>
+                    </div></Link>
+                </div>
+                <div className="footer-space-container">
+                </div>
+            </div>
+        ));
+    };
+    // Loading Function
+    const loading = () => {
+        return (
+            <div className="loading-container">
+                <div className="load-animation"></div>
+            </div>
+        )
+    }
+    return (
+        <>
+            {props.foods ? loaded() : loading()}
+        </>
+    )
+};
 
 export default Menu;
